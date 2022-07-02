c7de50b1e87326e804005c7528ed83135deb1b71
Neil de Carteret <git@lumphammer.net>
2022-07-02 19:03:57 +0100

    links

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,16 @@
+import Link from "next/link";
+import { Fragment } from "react"
+
 export default function FirstPost() {
-  return <h1>First Post!!!2</h1>
+  return (
+    <Fragment>
+      <h1>First Post!!!2</h1>
+      <h2>
+        <Link href="/"><a>Home</a></Link>
+      </h2>
+      <div>
+        <a href="/">Plain link home</a>
+      </div>
+    </Fragment>
+  );
 }
